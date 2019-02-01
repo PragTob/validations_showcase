a32cd8e4836536b950cdad5c453bc371a80b28f2
Tobias Pfeiffer <pragtob@gmail.com>
2019-02-01 17:11:31 +0100

    events adjusted

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,23 @@
 class Event < ApplicationRecord
+  before_validation :set_datetimes_to_date
+
+  DATE_TIMES = [
+    :crew_arrives_at,
+    :performers_arrive_at,
+    :open_at,
+    :starts_at,
+    :ends_at
+  ]
+
+  def set_datetimes_to_date
+    base_date = date
+
+    DATE_TIMES.each do |time_attribute|
+      original = public_send(time_attribute)
+      if original
+        adjusted_time = base_date.change hour: original.hour, min: original.min
+        self.public_send("#{time_attribute}=", adjusted_time)
+      end
+    end
+  end
 end
---
+++ scripts/set_datetimes.rb
@@ -0,0 +1,15 @@
+event =
+  Event.new(
+    name: "RUG::B",
+    location: "Dalia Research",
+    date: "07.02.2019",
+    crew_arrives_at: "18:55",
+    performers_arrive_at: "19:15",
+    open_at: "19:00",
+    starts_at: "19:30",
+    ends_at: "22:00"
+  )
+
+  p event
+  event.valid?
+  p event
